ad1db0dd8c03a77b5763f3922f22764906190fe9
SSKyun <tbvjzoshszld@gmail.com>
2023-03-29 19:37:05 +0900

    message

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Suspense } from 'react';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+
+  return (
+    <>
+    <Suspense fallback={<div>Loading...</div>}></Suspense>
+    <Component {...pageProps} />
+    </>
+  );
 }
